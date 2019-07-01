a1911a72245c7233a85288d8e787fda8f4aae2dc
CreeJee <000as000@naver.com>
2019-07-01 20:44:12 +0900

    real route test

---
+++ example/router&view/views/info.js
@@ -0,0 +1,23 @@
+import Component from '../../../src/Component.js';
+import fragment from '../../../src/Fragment.js';
+/**
+ * User page Component
+ */
+export default class User extends Component {
+    /**
+     * @param {Component.elementGenerator} u
+     * @return {HTMLElement|DocumentFragment}
+     */
+    render(u) {
+        return fragment(
+            u('nav', {},
+                u('ul', {},
+                    u('li', {},
+                        u('a', {text: 'main', href: './a'})
+                    )
+                )
+            ),
+            u('h1', {text: 'test'})
+        );
+    }
+}
