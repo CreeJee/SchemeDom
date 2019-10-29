badb88d0fe724bad95ba6a817a790ba860f97d82
CreeJee <000as00060@gmail.com>
2019-10-30 04:10:30 +0900

    ADDED DEFAULT EXPORT

---
+++ index.js
@@ -0,0 +1,4 @@
+import {create, bind, update, remove, Effect} from '/src/core/VNode.js';
+import {FixedType as _FixedType} from './core/FixedType.js';
+export const VNode = {create, bind, update, remove, Effect};
+export const FixedType = _FixedType;
